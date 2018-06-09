15d85bcbf24380457ba20eff52422868c346425e
theperfectfuel <robert@theperfectfuel.com>
2018-06-09 03:28:13 -0700

    add post header image to db

--- models/post.js
+++ models/post.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     title: String,
     content: String,
+    headerImg: {
+        type: String,
+        default: 'https://images.pexels.com/photos/1125273/pexels-photo-1125273.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+    },
     author: {
         firstName: String,
         lastName: String
@@ -21,6 +25,7 @@ postSchema.methods.serialize = function() {
     return {
         title: this.title,
         content: this.content,
+        headerImg: this.headerImg,
         author: this.authorName,
         id: this._id,
         created: this.created
